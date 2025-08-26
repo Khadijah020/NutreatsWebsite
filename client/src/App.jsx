aeba93d494fe4f11797eb6510da64f31ec6d391b
Khadijah Hasan <khadijahzaheer19@gmail.com>
2025-08-27 00:24:49 +0500

    base + frontend of home screen

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './components/navbar.jsx'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer.jsx'
+import { useAppContext } from './context/AppContext.jsx'
+import Login from './components/Login.jsx'
+
+const App = () => {
+
+  const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext()
+  return (
+    <div>
+      {isSellerPath ? null : <Navbar/>}
+      {showUserLogin ? <Login/> : null}
+      <Toaster />
+    <div className={`${isSellerPath} ? "" : 'px-6 md:px-16 lg:px-24 x1:px-32'`}>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </div>
+   { !isSellerPath && <Footer/>}
+    </div>
+  )
+}
+
+export default App
+
+
