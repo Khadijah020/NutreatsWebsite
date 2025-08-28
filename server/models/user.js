98cce595d0e69873d15836432952dff3185fd909
Khadijah Hasan <khadijahzaheer19@gmail.com>
2025-08-28 19:27:23 +0500

    user backend complete

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchem = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    cartItems: {type: Object, default: {}},
+
+
+}, {minimize: false} )
+
+const User = mongoose.models.user || mongoose.model('user', userSchem)
+
+export default User
